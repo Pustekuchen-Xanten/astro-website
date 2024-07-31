cb213c5f68fd265dc37c18ba0788af190bbedb6c
2TAP2B <steltner@pm.me>
2024-07-31 13:52:01 +0200

    rejndetest

--- src/navigation.js
+++ src/navigation.js
@@ -10,6 +10,10 @@ export const headerData = {
           href: getPermalink('/homes/saas'),
         },
         {
+          text: 'Test',
+          href: getPermalink('/test'),
+        },
+        {
           text: 'Startup',
           href: getPermalink('/homes/startup'),
         },
@@ -121,7 +125,7 @@ export const headerData = {
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Products',
       links: [
         { text: 'Features', href: '#' },
         { text: 'Security', href: '#' },
@@ -178,6 +182,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by ^^ <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
   `,
 };
